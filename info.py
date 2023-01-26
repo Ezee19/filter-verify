2e7d5e5b5634dc7f003b61eb226f13940ab84447
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-01-26 20:09:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,10 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
+API_ID = int(environ.get('API_ID', '17737898'))
+API_HASH = environ.get('API_HASH', 'ad762fe0516f367115ba651d929cf429')
+BOT_TOKEN = environ.get('BOT_TOKEN', "5580813031:AAGA15tdxcAVIsSZLKGS657DoDDFqtCRguc")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
@@ -47,7 +46,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '6435420271000057c2055fff45b9e30804ef896b')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))

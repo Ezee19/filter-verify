3156459b2518160abc11fd0ac537160c1446bb04
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-01-28 04:20:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Api-change
 fi
-cd /DQ-The-File-Donor
+cd /Api-change
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
